1cdcff45e6bda92b099c799fe281ded872eabfc9
helllomind <helllomind@hellomindmac.local>
2024-11-16 09:13:14 +0300

    rotation

--- dataset_generate.py
+++ dataset_generate.py
@@ -20,14 +20,18 @@ class TileImageGenerator:
         self.title = cv2.imread(self.title)
 
     def __iter__(self):
-        pass
+        while self.limit == 0 or self.count < self.limit:
+            yield self._process_image()
+            self.count += 1
 
-    def __next__(self):
+
+    def __next__(self) -> cv2.Mat:
         if self.limit == 0 or self.count < self.limit:
-            pass
+            self.count += 1
+            return self._process_image()
         else:
             raise StopAsyncIteration
-        self.count += 1
+        
 
     def __len__(self):
         return self.count
@@ -36,32 +40,34 @@ class TileImageGenerator:
 
     def _process_image(self) -> cv2.Mat:
         # Set a random title size as a proportion of the background
-        background_height, background_weight = self.background.shape[:2]
-        title_height, title_weight = self.title.shape[:2]
+        background_height, background_width = self.background.shape[:2]
+        title_height, title_width = self.title.shape[:2]
 
+        aspect_ratio = title_width / title_height
+        
         # Изменяем размер тайла
-        new_title_height = random.choice(range(
+        title_height = random.choice(range(
             int(background_height * self.title_min_proportion),
             int(background_height * self.title_max_proportion)
         ))
-        aspect_ratio = title_weight / title_height
-        new_title_weight = int(new_title_height * aspect_ratio)
-        resized_title = cv2.resize(self.title, (new_title_weight, new_title_height))
+        
+        title_width = int(title_height * aspect_ratio)
+        resized_title = cv2.resize(self.title, (title_width, title_height))
 
         # Set a random angle for the title
         angle = random.randint(0, 360)
-        center = (new_title_weight / 2, new_title_height / 2)
+        center = (title_width / 2, title_height / 2)
         rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1)
 
         # Рассчитываем новый размер холста для поворота
         cos = abs(rotation_matrix[0, 0])
         sin = abs(rotation_matrix[0, 1])
-        bound_w = int(new_title_height * sin + new_title_weight * cos)
-        bound_h = int(new_title_height * cos + new_title_weight * sin)
+        bound_w = int(title_height * sin + title_width * cos)
+        bound_h = int(title_height * cos + title_width * sin)
 
         # Корректируем матрицу поворота для нового холста
-        rotation_matrix[0, 2] += bound_w / 2 - new_title_weight / 2
-        rotation_matrix[1, 2] += bound_h / 2 - new_title_height / 2
+        rotation_matrix[0, 2] += bound_w / 2 - title_width / 2
+        rotation_matrix[1, 2] += bound_h / 2 - title_height / 2
 
         # Поворачиваем тайл с новым холстом
         rotated_title = cv2.warpAffine(
@@ -77,7 +83,7 @@ class TileImageGenerator:
         background_copy = self.background.copy()
 
         # Устанавливаем случайную позицию для тайла на фоне
-        x = random.randint(0, background_weight - bound_w)
+        x = random.randint(0, background_width - bound_w)
         y = random.randint(0, background_height - bound_h)
 
         # Выделяем область на фоне и используем маску для наложения
@@ -93,6 +99,7 @@ class TileImageGenerator:
 if __name__ == '__main__':
     backgrounds: list = get_backgrounds()
     first_generator = TileImageGenerator(background=backgrounds[1], title=get_titleset()[0][0], count=0, limit=10)
-    cv2.imshow('background', first_generator._process_image())
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    for image in first_generator:
+        cv2.imshow('image', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
